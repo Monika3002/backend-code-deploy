2e1f915d25fcef8b2de422fadcac8cdd75509e71
Monika3002 <monika25io038@satiengg.in>
2024-04-05 13:49:47 +0530

    Added a json file to send json data

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 4000
+const githubdata = require('./data.json')
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
   res.send('hello world')
@@ -15,6 +16,9 @@ app.get('/contact', (req, res) => {
   res.send('contact page')
 })
 
+app.get('/github',(req, res) => {
+  res.json(githubdata)
+})
 //this listens to the port is used to start the server without this the server will not start
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening at http://localhost:${process.env.PORT}`)
